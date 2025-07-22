ab10d94dbe26613b666182364aede22751203ab1
ach-melissa <infomely05@gmail.com>
2025-07-22 21:00:21 +0200

    new

--- src/pages/PageEntreprise/NosFormationsPage.jsx
+++ src/pages/PageEntreprise/NosFormationsPage.jsx
@@ -35,9 +35,9 @@ export default function NosFormationsPage() {
           {/* Left Section */}
           <div className={formationsStyles.leftSection}>
             <h2 className={formationsStyles.formationsSubtitle}>
-              Formations Par Domaine - Fiches techniques standard, modulables selon les besoins de votre entreprise
+              Formations Par Domaine
             </h2>
-
+            <h3 className={formationsStyles.formationsSubtitle0}> Fiches techniques standard, modulables selon les besoins de votre entreprise</h3>
             <div className={formationsStyles.formationsGrid}>
               <Link to="/formations/management" className={formationsStyles.formationCard}>
                 <img src={management} alt="Management Icon" className={formationsStyles.formationIcon} />
@@ -77,6 +77,12 @@ export default function NosFormationsPage() {
               </div>
             </div>
           </div>
+
+
+          
+          <div className={formationsStyles.verticalSeparator}></div>
+
+
 
           {/* Right Section */}
           <div className={formationsStyles.rightSection}>
